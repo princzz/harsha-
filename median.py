59a787c8b4bf0b25194cd150ce2516c9b753340d
princzz <harsha.k2565@gmail.com>
2025-08-04 11:47:09 +0530

    Add files via upload

---
+++ median.py
@@ -0,0 +1,19 @@
+import numpy as np
+a=np.array([[1,2,3,4]])
+b=np.array([[5,6,7,8]])
+sum=a+b
+sub=a-b
+product=a*b
+divisin=a/b
+meanvalue=np.mean(a)
+maxvalue=np.max(b)
+medianvalue=np.median(b)
+print("arr a",a)
+print("arr b:",b)
+print("sum:",sum)
+print("sub:",sub)
+print("product:",product)
+print("division:",divisin)
+print("mean:",meanvalue)
+print("median:",medianvalue)
+print("max:",maxvalue)
